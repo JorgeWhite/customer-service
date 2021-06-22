42859b1f35743785e3415c5fa9f48360a70f934f
Jorge Aguilar <jorgewhite@Jorges-MacBook-Air.local>
2021-06-22 15:16:59 -0500

    first commit

---
+++ src/main/java/com/dws/custormerservice/CustormerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dws.custormerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustormerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustormerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dws/custormerservice/CustormerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dws.custormerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustormerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
