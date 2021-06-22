2975516fa629e500ca876892e3fad98ddefd37f6
Jorge Aguilar <jorgewhite@Jorges-MacBook-Air.local>
2021-06-22 17:24:37 -0500

    Feature CRUD_Region add dto region
    Se creó el dto de región
    
    Funcionalidad CRUD Region
    API: Customer
    Responsable: Jorge Aguilar

---
+++ src/main/java/com/dws/custormerservice/dto/Region.java
@@ -0,0 +1,11 @@
+package com.dws.custormerservice.dto;
+
+    public class Region {
+        private int id;
+        private String region;
+    
+        public Region(int id, String region){
+            this.id = id;
+            this.region = region;
+        }
+    }
